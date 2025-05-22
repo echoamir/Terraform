8bd8177ef8413488037e4310bff049291d28be65
echoamir <kolahiamir1@gmail.com>
2025-05-22 13:53:06 +0330

    config minio providers files

---
+++ Minio/providers.tf
@@ -0,0 +1,15 @@
+terraform {
+  required_providers {
+    minio = {
+      source = "aminueza/minio"
+      version = "3.3.0"
+    }
+  }
+}
+
+provider "minio" {
+  minio_server   = "MINIO_API_DOMAIN"
+  minio_user     = "MINIO_ACCESS_KEY"
+  minio_password = "MINIO_SECRET_KEY"
+  minio_ssl      = true
+}
