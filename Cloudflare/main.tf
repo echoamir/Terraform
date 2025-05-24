782d0a65334ed41032c4bbfa74f1b5baf8144a3a
echoamir <kolahiamir1@gmail.com>
2025-05-24 08:37:48 +0330

    udpate cloudflare

--- Cloudflare/main.tf
+++ Cloudflare/main.tf
@@ -1,6 +1,5 @@
 resource "cloudflare_dns_record" "a_record" {
   zone_id = var.zone_id
-  comment = "Domain verification record"
   content = var.a_record_value
   name = var.a_record_name
   proxied = false
