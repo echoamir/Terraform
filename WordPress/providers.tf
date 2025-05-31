93d91ebd8f48687f453017dc538009a49c91d981
echoamir <kolahiamir1@gmail.com>
2025-05-31 08:36:52 +0330

    update wordpress

--- WordPress/providers.tf
+++ WordPress/providers.tf
@@ -1,3 +1,12 @@
 provider "docker" {
   host = "unix:///var/run/docker.sock"
 }
+
+terraform {
+  required_providers {
+    docker = {
+      source  = "kreuzwerker/docker"
+      version = "~> 3.0"
+    }
+  }
+}
