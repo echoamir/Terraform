0f3c9fb9f04a901179c3bf56308284a1fe0cbc22
echoamir <kolahiamir1@gmail.com>
2025-06-26 09:52:00 +0330

    update minio session

--- Minio/providers.tf
+++ Minio/providers.tf
@@ -9,6 +9,7 @@ terraform {
 
 provider "minio" {
   minio_server   = "MINIO_API_DOMAIN"
+  minio_region   = "us-east-1"
   minio_user     = "MINIO_ACCESS_KEY"
   minio_password = "MINIO_SECRET_KEY"
   minio_ssl      = true
